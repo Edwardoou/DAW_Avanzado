e7806e04608cdbb7d352d2df0f6c03da2d2308c9
Alex Ramos <alex_er@hotmail.com>
2022-03-28 11:41:15 -0500

    feat: :wrench: added routes, finished
    Tarea de lab 2 acabada, rutas añadidas que editan un array
    
    posPassword y Login no tienen relacion con el array

--- semana02_rutasExpressLab/src/controllers/userController.js
+++ semana02_rutasExpressLab/src/controllers/userController.js
@@ -2,6 +2,30 @@
  * *Nuestro archivo que contiene la logica de la app
  */
 
+const DATA = [
+  {
+    id: 1,
+    name: "Trika",
+    lastname: "World",
+    email: "trika@mail.com",
+    password: "trika123",
+  },
+  {
+    id: 2,
+    name: "Alex",
+    lastname: "Ramos",
+    email: "alex@mail.com",
+    password: "alex123",
+  },
+  {
+    id: 3,
+    name: "Jesus",
+    lastname: "Lujan",
+    email: "sideral@mail.com",
+    password: "sideral123",
+  },
+];
+
 export const getIndex = (req, res) => {
   // cuando vamos a construir un API las respuestas
   // que enviemos deben ser en formato JSON
@@ -16,8 +40,54 @@ export const postLogin = (req, res) => {
   res.json({
     data: {
       type: "login",
-      email: email,
-      password: password,
+      email,
+      password,
     },
   });
+};
+
+export const postSignUp = (req, res) => {
+  const { name, lastname, email, password } = req.body;
+  const id = DATA.length + 1;
+  DATA.push({ id, name, lastname, email, password });
+  res.json({
+    data: {
+      type: "signup",
+      value: DATA[id - 1],
+    },
+  });
+};
+
+export const getUserId = (req, res) => {
+  const { id } = req.params;
+  let index = id - 1; //restar 1 para la busqueda en el array DATA que empieza en 0
+  res.json({
+    data: {
+      type: "show",
+      value: DATA[index],
+    },
+  });
+};
+
+export const postPassword = (req, res) => {
+  const { email } = req.body;
+  res.json({
+    email,
+  });
+};
+
+export const putUser = (req, res) => {
+  const { id } = req.params;
+  const { name, lastname, email, password } = req.body;
+  let index = id - 1;
+  DATA[index] = { id, name, lastname, email, password }; //actualizar objeto segun INDEX de la DATA
+  res.json(DATA); //toda la DATA actualizada
+};
+
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+  let index = id - 1;
+  //Nuevo array eliminando 1, pasando el indice ya restado para que se elimine a si mismo
+  DATA.splice(index, 1);
+  res.json(DATA); //nueva DATA
 };
--- semana02_rutasExpressLab/src/network.js
+++ semana02_rutasExpressLab/src/network.js
@@ -1,11 +1,24 @@
 //* Se llama network y no routes porque cada uno tendra su js de ruta, nombre routes esta deprecated
 import { Router } from "express";
-import { getIndex, postLogin } from "./controllers/userController.js";
+import {
+  deleteUser,
+  getIndex,
+  getUserId,
+  postLogin,
+  postPassword,
+  postSignUp,
+  putUser,
+} from "./controllers/userController.js";
 
 const router = Router();
 
 //rutas
 router.route("/").get(getIndex);
-router.route("/").get(postLogin);
+router.route("/login").post(postLogin);
+router.route("/sign-up").post(postSignUp);
+router.route("/show-user/:id").get(getUserId);
+router.route("/reset-password").post(postPassword);
+router.route("/update-user/:id").put(putUser);
+router.route("/delete-user/:id").delete(deleteUser);
 
 export default router;
