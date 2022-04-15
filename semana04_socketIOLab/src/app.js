0517998a95d4993718da3edf1ef0e5cf860e442b
Alex Ramos <aletsoshi@outlook.com>
2022-04-14 21:17:20 -0500

    fix: :fire: SocketIO Lab ready
    Se añadio del repo del profe los comments, trajo buenos comentarios y se arreglo para el lab

--- semana04_socketIOLab/src/app.js
+++ semana04_socketIOLab/src/app.js
@@ -3,7 +3,16 @@ import express from "express";
 export const app = express();
 
 app.use(express.static("src/public"));
+//? Esto sirve para poder leer el body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+//Chat
+app.get("/chat", (req, res) => {
   res.sendFile(__dirname + "/public/chat.html");
+});
+
+//Comentarios
+app.get("/comments", (req, res) => {
+  res.sendFile(__dirname + "/public/comments.html");
 });
